5ae45ccb3005a5763226762bf7dbc0320d90d6bb
teamatash <65574416+teamatash@users.noreply.github.com>
2020-05-20 20:46:01 -0400

    Update 00 Merge Data.py

--- 00 Merge Data.py
+++ 00 Merge Data.py
@@ -3,14 +3,16 @@ import pandas as pd
 import zipfile
 import os
 
-
+#opens zip
 zf = zipfile.ZipFile('Datasets 690.zip')
 df = {}
 i = 1
+#adds all the different datasets from 2018 to 2020 into one csv file
 for year in range(2018,2020):
     for month in range(1,13):
         df[i] = pd.read_csv(zf.open('On_Time_Marketing_Carrier_On_Time_Performance_(Beginning_January_2018)_%s_%s.csv'%(year,month)))
         i += 1
 frames = [df[i] for i in range(1,25)]
 df = pd.concat(frames,ignore_index=True)
+#dataframe into a csv
 df.to_csv('data.csv')
